4293dc880a420533b0b218f60387912a221c961f
Emmanuel Danso <102678692+emmadanso@users.noreply.github.com>
2024-06-21 08:52:41 +0000

    Add files via upload

---
+++ Inheritance and Method Overriding/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace InheritanceExample
+{
+    class Animal
+    {
+        public virtual void MakeSound()
+        {
+            Console.WriteLine("Some generic sound");
+        }
+    }
+
+    class Dog : Animal
+    {
+        public override void MakeSound()
+        {
+            Console.WriteLine("Bark");
+        }
+    }
+
+    class Cat : Animal
+    {
+        public override void MakeSound()
+        {
+            Console.WriteLine("Meow");
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Animal genericAnimal = new Animal();
+            genericAnimal.MakeSound();
+
+            Dog dog = new Dog();
+            dog.MakeSound();
+
+            Cat cat = new Cat();
+            cat.MakeSound();
+        }
+    }
+}
---
+++ Interfaces/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace InterfacesExample
+{
+    interface IMovable
+    {
+        void Move();
+    }
+
+    class Car : IMovable
+    {
+        public void Move()
+        {
+            Console.WriteLine("Car is moving");
+        }
+    }
+
+    class Bicycle : IMovable
+    {
+        public void Move()
+        {
+            Console.WriteLine("Bicycle is moving");
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IMovable car = new Car();
+            car.Move();
+
+            IMovable bicycle = new Bicycle();
+            bicycle.Move();
+        }
+    }
+}
+
